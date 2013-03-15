0ac8a2ae2ad5826ba489709648d818dd43b5e6dd
Dmitry Kungurov <125030370+yepsconnect@users.noreply.github.com>
2013-03-15 20:29:05 +0400

    update random js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var quotes = ["Цитата 1", "Цитата 2", "Цитата 3", "Цитата 4", "Цитата 5"];
+var allQuotes = ["Цитата 1", "Цитата 2", "Цитата 3", "Цитата 4", "Цитата 5"];
+
+var shownQuotes = [];
 
 function getRandomQuote() {
-  var randomIndex = Math.floor(Math.random() * quotes.length);
-  return quotes[randomIndex];
+  if (shownQuotes.length === 0) {
+    shownQuotes = allQuotes.slice(0);
+  }
+
+  var randomIndex = Math.floor(Math.random() * shownQuotes.length);
+  return shownQuotes.splice(randomIndex, 1)[0];
 }
 
 function updateQuote() {
